746989e4309c9214ccd9a94d0aaf744d4492908a
Christopher Dunn <christopherdunnlaw@gmail.com>
2019-05-08 15:32:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(greeting)
    if
-      greeting == 'I LOVE YOU GRANDMA!'
+      greeting === 'I LOVE YOU GRANDMA!'
       puts 'I LOVE YOU TOO PUMPKIN!'
   elsif
     greeting == greeting.upcase
