bbc0b67b57b3dcfdd2290e300a90e76b3ccae472
Christopher Dunn <christopherdunnlaw@gmail.com>
2019-05-08 15:16:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(greeting)
     greeting == 'I LOVE YOU GRANDMA!'
     puts 'I LOVE YOU TOO PUMPKIN!'
    elsif greeting == greeting.downcase
-   puts 'HUH?! SPEAK UP SONNY!'
+   puts 'HUH?! SPEAK UP, SONNY!'
    else
   puts 'NO, NOT SINCE 1938!'
 end
